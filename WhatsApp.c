d8dbd41305b58156320fb456b2aab0c7d7d1e72c
Mich14 <michygc14@gmail.com>
2013-09-08 00:04:49 -0600

    Cambios en el fork

--- WhatsApp.c
+++ WhatsApp.c
@@ -20,7 +20,7 @@ void error(const char *msg)
 int main(int argumentos, char *args[])
 {
 	//Declaracion de variables
-	int socket1, nuevoSocket, numPuerto, pid, mensaje, Estado;   
+	int socket1, nuevoSocket, numPuerto, pid, mensaje;   
 	socklen_t clilen;
 	char buffer[256];
 
@@ -31,7 +31,7 @@ int main(int argumentos, char *args[])
 	}
 	
 	numPuerto = atoi(args[2]);
-	
+		
 	pid = fork();
 	
 	//Si el id es menor a cero muestra un error en el fork
@@ -42,11 +42,12 @@ int main(int argumentos, char *args[])
 	//Si el id es igual a 0 se cierra el socket y se abre una nueva comunicacion
 	if (pid == 0)
 	{
-		printf("Entre a cliente \n");
+		
 		struct sockaddr_in dir_servidor;             
 		struct hostent *servidor;
 		
 		while (1){
+			sleep(2);
 			
 			//Se crea el socket
 			socket1 = socket(AF_INET, SOCK_STREAM, 0);
@@ -76,7 +77,7 @@ int main(int argumentos, char *args[])
 			
 			//Verifica que pueda establecer la conexion
 			if (connect(socket1,(struct sockaddr *)&dir_servidor,sizeof(dir_servidor)) < 0) 
-				error("ERROR de conexión");
+				error("ERROR de conexión clie");
 			
 			printf("Por favor, introduzca el mensaje:");
 			
@@ -100,19 +101,20 @@ int main(int argumentos, char *args[])
 			//Si se produce un error al leer el msj, se muestra el error
 			if (mensaje < 0)
 				error("ERROR al leer del socket");
-
-			printf("%s\n",buffer);
+			
+			
 			close(socket1);
-			//sleep (1);
+	
 		}
 		return 0;
 	}
 		
 	else {  //Servidor
+		
 		struct sockaddr_in dir_servidor, dir_cliente;
 		
 		socket1 = socket(AF_INET, SOCK_STREAM, 0);
-		printf("Entre a servidor \n");
+		
 		//validacion en caso de no se pueda establecer el socket
 		if (socket1 < 0)
 			error("ERROR de apertura de socket");
@@ -122,7 +124,7 @@ int main(int argumentos, char *args[])
 		//numPuerto obtine el numero de puerto ingresado por el usuario
 		
 		dir_servidor.sin_family = AF_INET;                  //Protocolo TCP
-		dir_servidor.sin_addr.s_addr = inet_addr ("192.168.16.3");          //Representa la direccion ip
+		dir_servidor.sin_addr.s_addr = INADDR_ANY;          //Representa la direccion ip
 		dir_servidor.sin_port = htons(numPuerto);           //Representa el puerto
 
 		//Verifica la conexion
@@ -136,31 +138,28 @@ int main(int argumentos, char *args[])
 			//Tamaño de bytes que ocupa la direccion cliente
 			clilen = sizeof(dir_cliente);
 			
-			printf ("Hasta aui creo estar bn \n");
 			nuevoSocket = accept(socket1,(struct sockaddr *) &dir_cliente, &clilen);
 			if (nuevoSocket < 0)
 			error("ERROR en aceptar.....");
-			sleep (2);
+			
+			sleep (5);
+			
 			bzero(buffer,256);
-	
+			
 			//Lee el mensaje
-			mensaje = read(socket1,buffer,255);
+			mensaje = read(nuevoSocket,buffer,255);
 			//Muestra el error en caso de que se produzca
 			if (mensaje < 0)
 				error("ERROR al leer del socket");
 			
 			//Muestra el mensaje recibido
 			printf("Aquí está el mensaje: %s\n",buffer);
-			mensaje = write(socket1,"Tengo el mensaje",18);
-			
-			//Si no puede leer el msj muestra el error
-			if (mensaje < 0)
-				error("ERROR al escribir en el socket");
 			
 			close(nuevoSocket);
-			close(socket1);
+			
 			
 		}	
 	}
 	return 0;
 }
+
