b6d67e8309745024cf7b1cff13c270bdec888815
jfabricio2010 <juanfab2010@gmail.com>
2013-09-08 01:43:17 -0500

    Actualización del archivo, ahora verifica el usuario que se desea buscar en la lista de contactos.

--- Escribir.c
+++ Escribir.c
@@ -1,9 +1,13 @@
-# include <stdio.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
 int escribir( ){
 	FILE * archivo;
 	char* users = "listaContactos.txt";//Apunta al archivo en memoria
 	char user[30];
+	char ip[30];
+	char puerto[30];
 	
 	archivo = fopen(users,"a");//agrega al final del archivo
 	if(archivo==NULL) return -1;
@@ -11,8 +15,14 @@ int escribir( ){
 	
 	printf("Escriba el Nombre de usuario:");
 	scanf(" %[^\n]",user); //Lee del teclado cosas con espacios hasta presionar enter
+	printf("Escriba el ip:");
+	scanf(" %[^\n]",ip);
+	printf("Escriba el puerto:");
+	scanf(" %[^\n]",puerto);
 	
 	fprintf(archivo,"%s\n",user);
+	fprintf(archivo,"%s\n",ip);
+	fprintf(archivo,"%s\n",puerto);
 	fclose(archivo);
 	
 
@@ -20,50 +30,94 @@ int escribir( ){
 	}
 	
 	
-int leer2(){
-	static const char users[] = "listaContactos.txt";
-	FILE *file = fopen ( users, "r" );
-	if ( file != NULL )
-	{
-		char linea [30]; //Se almacena cada linea
-		while ( fgets ( linea, sizeof linea, file ) != NULL ) //Lee una linea
-		{
-			fputs ( linea, stdout ); //Escribe la linea en pantalla
-		}
-		fclose ( file );
-	}
-	else
-	{
-		perror ( users ); //Me indica el tipo de error lo transforma a mensaje
-	}
-	return 0;
-}
+
 	
-int leer(){
-	char listacont[30];
-	int indicafin=0;
-	FILE * archivo;
-	char * users= "listaContactos.txt";
-		
-	archivo = fopen(users,"r");// Leer desde el .txt
-	if (users==NULL)return -1;
-		
-	printf("Contactos: \n");
-		
-	indicafin=fscanf(archivo,"%[^\n]",&listacont);//Almacena el EOF en indicafin
-		
-	while(indicafin!=EOF){//Mientras sea diferende de EOF imprime linea por linea hasta que encuentra enter
-		printf("%s\n",listacont);//Recibe puntero a la lista de contactos temporal y la imprime hasta salto de linea.
-		indicafin=fscanf(archivo," %[^\n]", &listacont);
+int contar(char arreglo[]){
+	int cont =0;
+	while(arreglo[cont]!='\0'){
+		cont ++;
 	}
-	fclose(archivo);
-	//Cierra el archivo	
+	
+	return cont;
 }
+
+int Comparar(char arreglo1[],char arreglo2[]){
+	int x = contar(arreglo1);
+	int y = contar(arreglo2);
+	int cont=0;
+	if (x!= y){
+		//printf("falso0");
+		return 1;	
+	}
+         	 		
+	while(cont!=x-1){
+		printf("%c, %c",arreglo1[cont],arreglo2[cont] );
+		if (arreglo1[cont]==arreglo2[cont]){
+			cont++;
+			printf("Si ");
+		}
+		else{
+			//printf("falso");
+		return 1;
+		}
+	}
+		//printf("verdadero");
+		return 0;
+		
+	}
+	
+
+	 
+	
+ int lee(char usuario[]){
+	 FILE *archivo;
+		
+        char caracteres[30];
+        static char user[30];
+        static char ip[30];
+        static char puerto[30];
+		int i=0;
+        archivo = fopen("listaContactos.txt","r");
+ 
+        if (archivo == NULL)
+                exit(1);
+ 
+        printf("\nEl contenido del archivo de prueba es \n\n");
+        while (feof(archivo) == 0)
+        {
+                fgets(caracteres,30,archivo);
+				printf("%s",caracteres);
+                if(((Comparar(caracteres,usuario))==0)||(i>0)){
+					if(i==0){strcpy(user,caracteres);
+					printf("ajjajaja","%s",user);}
+					if(i==1)strcpy(caracteres,ip);
+					if(i==2)strcpy(caracteres,puerto);
+					}i++;
+        }
+ 
+        fclose(archivo);
+        return 0;
+	 
+	 
+	 
+	 }
+ 
+
+
 
 
 int main(int argc, char *argv[]){
+	//char usuario[30];
+	//printf("Para: ");
+	//scanf("%c",&usuario);
+
+	//escribir();
+	/*******************************************/
+	char user[30];
+	printf("Introduzca usuario :");
+	scanf("%s",&user);
+	lee(user);
 	
-escribir();
-leer();
-		
+        
+			
 	}
