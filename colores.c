feb41c0faca81427d6d2eb2f048b62185c6cd889
CarlosACF <carlos-cf93@hotmail.com>
2013-09-07 18:36:48 -0600

    Posibles colores para los printf

--- colores.c
+++ colores.c
@@ -2,9 +2,9 @@
 
 void main ()
 {
-printf ("\033[30m Salida en color: Negro\n");
-printf ("\033[31m Salida en color: Rojo\n");
-printf ("\033[32m Salida en color: Verde\n");
-printf ("\033[33m Salida en color: Amarillo\n");
-printf ("\033[34m Salida en color: Azul\n");
+printf ("\033[33m amarillo\n");
+printf ("\033[34m azul\n");
+printf ("\033[31m rojo\n");
+printf ("\033[32m verde\n");
+
 }
