dd6b1eb00ea1fe123ec39163f5eef8af149a8100
CarlosACF <carlos-cf93@hotmail.com>
2013-09-07 18:21:51 -0600

    cambiar colores los printf

---
+++ colores.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+void main ()
+{
+printf ("\033[30m Salida en color: Negro\n");
+printf ("\033[31m Salida en color: Rojo\n");
+printf ("\033[32m Salida en color: Verde\n");
+printf ("\033[33m Salida en color: Amarillo\n");
+printf ("\033[34m Salida en color: Azul\n");
+}
